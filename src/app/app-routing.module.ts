fff0061eb77b86c213601dc725a3c1db59b45153
emanoelcampos <emanoel.cv@gmail.com>
2022-10-21 11:02:50 -0300

    criando modulo produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)
+  },
+  {
+    path: '',
+    redirectTo: 'produtos', pathMatch: 'full'
+  }
+
+
 ];
 
 @NgModule({
