44ec270a042c2176957ec983db1e1cb390c761b9
emanoelcampos <emanoel.cv@gmail.com>
2022-10-21 11:46:01 -0300

    configurando template e estilos de pagina 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'produtos', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NaoEncontradoComponent
   }
 
 
