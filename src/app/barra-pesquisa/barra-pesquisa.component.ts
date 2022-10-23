49ea6e6148af113ab33849f98c66dbf94279cd03
emanoelcampos <emanoel.cv@gmail.com>
2022-10-23 03:33:03 -0300

    configurando barra de pesquisa

--- src/app/barra-pesquisa/barra-pesquisa.component.ts
+++ src/app/barra-pesquisa/barra-pesquisa.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraPesquisaComponent implements OnInit {
 
-  constructor() { }
+  descricao = '';
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  pesquisar() {
+    if (this.descricao) {
+      this.router.navigate(['produtos'], {queryParams:{ descricao: this.descricao }})
+      return;
+    }
+    this.router.navigate(['produtos']);
   }
 
 }
